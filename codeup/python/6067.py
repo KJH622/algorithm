a3333862c0518cfa1af888432d82086cbb3ba23e
jihyun <russee201916545@gmail.com>
2025-10-07 22:37:20 +0900

    CodeUp 6067

---
+++ codeup/python/6067.py
@@ -0,0 +1,13 @@
+# 정수 1개 입력받아 분류하기
+
+a = int(input())
+if a < 0:
+    if a % 2 == 0:
+        print("A")
+    else:
+        print("B")
+else:
+    if a % 2 == 0:
+        print("C")
+    else:
+        print("D")
