6823614e4a61c02895f25c08574ba84a01644c9f
jihyun <russee201916545@gmail.com>
2025-11-01 17:03:57 +0900

    BAEKJOON 5554

---
+++ baekjoon/05/5554.py
@@ -0,0 +1,10 @@
+# 심부름 가는 길
+
+to_school = int(input())
+to_PC = int(input())
+to_study = int(input())
+to_home = int(input())
+
+sum = to_school + to_PC + to_study + to_home
+print(sum//60)
+print(sum%60)
