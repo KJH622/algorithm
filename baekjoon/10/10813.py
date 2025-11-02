ea7d2625aec1f23d40dc6be120b2555c20efca1c
jihyun <russee201916545@gmail.com>
2025-11-02 16:23:56 +0900

    BAEKJOON 10813

---
+++ baekjoon/10/10813.py
@@ -0,0 +1,11 @@
+# 공 바꾸기
+
+n, m = map(int, input().split())
+basket = list(range(1, n+1))
+
+for i in range(m):
+    i, j = map(int, input().split())
+    basket[i-1], basket[j-1] = basket[j-1], basket[i-1]
+
+for i in range(n):
+    print(basket[i], end=' ')
