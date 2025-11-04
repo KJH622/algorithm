0ea508ec3b9212b2185a93ab0d3bb20d16f107b6
jihyun <russee201916545@gmail.com>
2025-11-05 00:25:26 +0900

    BAEKJOON 2501

---
+++ baekjoon/02/2501.py
@@ -0,0 +1,14 @@
+# 약수 구하기
+
+lst = []
+
+n, k = map(int, input().split())
+
+for i in range(1, n+1):
+    if n % i == 0:
+        lst.append(i)
+
+if len(lst) >= k:
+    print(lst[k-1])
+else:
+    print("0")
