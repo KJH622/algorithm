d281e347ebd01222bafb4d3db0317ad8cc9e1596
jihyun <russee201916545@gmail.com>
2025-11-07 20:28:05 +0900

    BAEKJOON 3460

---
+++ baekjoon/03/3460.py
@@ -0,0 +1,23 @@
+# 이진수
+
+# 양의 정수 n이 주어진다.
+# 이 수를 이진수로 바꾸었을 때, 1
+
+import sys
+
+input = sys.stdin.readline
+
+T = int(input())
+
+for _ in range(T):
+    n = int(input())
+    pos = 0
+    result = []
+
+    while n > 0:
+        if n & 1:          # 현재 비트가 1이라면
+            result.append(str(pos))
+        n >>= 1             # 다음 비트로 이동
+        pos += 1
+
+    print(' '.join(result))
