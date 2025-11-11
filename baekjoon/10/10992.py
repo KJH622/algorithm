571846d62afaf20fa63462dda369fb0935a878c0
jihyun <russee201916545@gmail.com>
2025-11-11 19:45:58 +0900

    BAEKJOON 10992

---
+++ baekjoon/10/10992.py
@@ -0,0 +1,11 @@
+# 별 찍기 - 17
+
+n = int(input())
+
+for i in range(1, n+1):
+    if i == 1:
+        print(" " * (n-i) + "*")
+    elif i == n:
+        print(" " * (n-i) + "*" * (2 * i - 1))
+    else:
+        print(" " * (n-i) + "*" + ' ' * (2 * i - 3) + "*")
