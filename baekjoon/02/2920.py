631b24a70eeb4f40ae2cd5d8fb548aa17f5de8c9
jihyun <russee201916545@gmail.com>
2025-11-14 16:33:05 +0900

    BAEKJOON 2920

---
+++ baekjoon/02/2920.py
@@ -0,0 +1,10 @@
+# 음계
+
+music = list(map(int, input().split()))
+
+if music == sorted(music):
+    print("ascending")
+elif music == sorted(music, reverse = True):
+    print("descending")
+else:
+    print("mixed")
