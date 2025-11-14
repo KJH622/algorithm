ce621f910d80df752d5ff57777525739332a55a0
jihyun <russee201916545@gmail.com>
2025-11-14 17:22:18 +0900

    BAEKJOON 10811

---
+++ baekjoon/10/10811.py
@@ -0,0 +1,10 @@
+# 바구니 뒤집기
+
+n, m = map(int, input().split())
+basket = [i for i in range(1, n+1)]
+
+for _ in range(m):
+    i, j = map(int, input().split())
+    basket[i-1:j] = reversed(basket[i-1:j]) # 뒤집기
+
+print(*basket) # 언패킹
